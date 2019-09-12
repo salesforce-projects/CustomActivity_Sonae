3eaa9ee32885ad3ae4f140ab94125c0d9b01500e
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-09-12 15:54:16 +0100

    Update activity.js

--- journey-builder-activity-template-master/routes/activity.js
+++ journey-builder-activity-template-master/routes/activity.js
@@ -138,7 +138,7 @@ exports.execute = function (req, res) {
 								}
 
 								const req2 = http.request(options, (res) => {
-								  console.log('statusCode: ' + res.statusCode)
+								  console.log('statusCode SFMC: ' + res.statusCode)
                                     
 								  res.on('data', (d) => {
 									process.stdout.write(d)
