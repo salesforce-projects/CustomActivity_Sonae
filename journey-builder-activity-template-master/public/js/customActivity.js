78d70495553653cd470e73e3bc0ce99f31374a0f
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-09-30 14:02:12 +0100

    added MultiSelect jQuery

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -8,7 +8,9 @@ define([
     var connection = new Postmonger.Session();
     var authTokens = {};
     var payload = {};
-	var argumentos;
+    var argumentos;
+    var camposDE = [];
+
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
@@ -60,79 +62,17 @@ define([
             console.log('*** Schema ***', JSON.stringify(data['schema']));
             var JsonParsed = data['schema'];
             var key;
-            var campos = [];
             for (var i = 0, len = data['schema'].length; i < len; ++i) {
                 key = JsonParsed[i].key;
-                campos.push(key.substr(key.lastIndexOf(".") + 1));
+                camposDE.push(key.substr(key.lastIndexOf(".") + 1));
             }
-            campos.forEach(function(campo) {
+            camposDE.forEach(function(campo) {
                 console.log("CAMPO DA DATA EXTENSION -> " + campo);
             });
             //console.log("KEY NA POSIÇAO 0 -> " + JsonParsed[0].key);
          });
     }
-		
-		
-		
-        /*console.log(data);
-        if (data) {
-            payload = data;
-        }
-        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        console.log(inArguments);
-
-        $.each(inArguments, function (index, inArgument) {
-			var variaveis;
-			$.each(inArgument, function (key, val) {
-				variaveis = variaveis + val; 
-				
-            });
-			
-		var bodyText = {
-		"grant_type":"client_credentials",
-		"client_id":"cfly1ym6xx6y34jbqw0idypq",
-		"client_secret":"FXaTXByn5UyO7r1equQ8OwxU",
-		"variaveis" : variaveis
-		};
-		var $j = jQuery.noConflict();
-		var token;
-		$j.support.cors = true;
-		$j.ajax({
-		type: "POST",
-		url: process.env.postURL,
-		headers: {
-			'Origin' : process.env.postURL,
-			'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With',
-			'Access-Control-Allow-Origin' : '*',
-			'Access-Control-Allow-Methods' : 'GET, POST, PUT',
-			'Content-Type': 'application/json'
-		},
-		crossDomain: true,
-		data: JSON.stringify(bodyText),
-		dataType: 'json',
-		success: function(responseData, status, xhr) {
-			console.log(responseData);
-		},
-		error: function(request, status, error) {
-			console.log(request.responseText);
-		}});
-			 
-        });
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });*/
-
+    
     function onGetTokens(tokens) {
         console.log(tokens);
         authTokens = tokens;
