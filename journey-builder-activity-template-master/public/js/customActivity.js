fc7606e2d03760afb2f734766ff6f69a25ac1447
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-10-10 14:39:43 +0100

    Update customActivity.js

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -1,263 +1,132 @@
-'use strict';
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
+    'use strict';
 
-var util = require('util');
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+    var argumentos;
+    var camposDE = [];
+    var camposSelected = [];
 
-var respostaAuth;
-var path;
-var host;
-var decodedArgs;
-var journeyInfoJson;
-// Deps
-const Path = require('path');
-const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
-const ET_Client = require('sfmc-fuelsdk-node');
-const FuelRest = require('fuel-rest');
-const options = {
-	auth: {
-		// options you want passed when Fuel Auth is initialized
-		clientId: 'cfly1ym6xx6y34jbqw0idypq',
-		clientSecret: 'FXaTXByn5UyO7r1equQ8OwxU'
-	}
-};
-const RestClient = new FuelRest(options);
-var util = require('util');
-var http = require('https');
-var jsonSize = require('json-size'); 
+    $(window).ready(onRender);
 
-exports.logExecuteData = [];
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
 
-function logData(req) { 
-    exports.logExecuteData.push({
-        body: req.body,
-        headers: req.headers,
-        trailers: req.trailers,
-        method: req.method,
-        url: req.url,
-        params: req.params,
-        query: req.query,
-        route: req.route,
-        cookies: req.cookies,
-        ip: req.ip,
-        path: req.path,
-        host: req.hostname,
-        fresh: req.fresh,
-        stale: req.stale,
-        protocol: req.protocol,
-        secure: req.secure,
-        originalUrl: req.originalUrl
-    });
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.hostname);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
-}
+    connection.on('clickedNext', save);
 
-/*
- * POST Handler for / route of Activity (this is the edit route).
- */
-exports.edit = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-	res.status(200).send('Edit');
-};
-
-/*
- * POST Handler for /save/ route of Activity.
- */
-exports.save = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-	res.status(200).send('Save');
-};
-
-/*
- * POST Handler for /execute/ route of Activity.
- */
-exports.execute = function (req, res) {
-    
-    // example on how to decode JWT
-    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-        // verification error -> unauthorized request
-        if (err) {
-            return res.status(401).end();
-        }
-			
-        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            decodedArgs = decoded.inArguments[0];
-            const data = JSON.stringify(decodedArgs)
-
-								const options = {
-								  hostname: 'postb.in',
-								  path: '/1570714229122-4677748421672',
-								  method: 'POST',
-								  headers: {
-									'Content-Type': 'application/json',
-									'Content-Length': jsonSize(decoded.inArguments[0])
-								  }
-								}
-
-								const req2 = http.request(options, (res) => {
-								  console.log('statusCode: ${res.statusCode}')
-
-								  res.on('data', (d) => {
-									process.stdout.write(d)
-								  })
-								}) 
-
-								req2.on('error', (error) => {
-								  console.error(error)
-								})
-
-								req2.write(data);
-								req2.end();
-            /*----------------------ACESSTOKEN-----------------------*/
-//+ decoded.inArguments[0].DefinitionId +
-            
-
-			res.status(200).send('Execute');
-        } else {
-            console.error('inArguments invalid.');
-            return res.status(400).end();
-        }
-    });
-};
-
-function historyJourney(access){
-            /*----------------------HISTORYJOURNEY-------------------*/
-            
-            var body = {
-                "from": 0,
-                "size": 100,
-                "filter": {
-                    "fquery": {
-                        "query": {
-                            "query_string": {	
-                                "query": "(definitionId:" + decodedArgs.DefinitionId + ") AND(transactionTime:[2019-09-05T13:20:45.101Z TO *])"
-                            }
-                        }
-                    }
-                }
-            };
-            var respostaJourneyInfo;
-            var data = JSON.stringify(body)
-
-            
-            var options = {
-                hostname: 'mcdgsnqlh4ybg-9cyt895ypwkxh0.rest.marketingcloudapis.com',
-                path: '/interaction/v1/interactions/traceevents/search',
-                method: 'POST',
-                headers: {
-                'Authorization' : 'Bearer ' + access,
-                'Content-Type': 'application/json',
-                'Content-Length': jsonSize(body)
-                }
-            }
-
-            const req3 = http.request(options, (res) => {
-                console.log('statusCode: ' + res.statusCode)
-                
-                let chunks = [];
-                res.on('data', (d) => {
-                chunks.push(d);
-                }).on('end', function() {
-
-                var data = Buffer.concat(chunks);
-                var datastring = data.toString();
-                data = datastring.replace(/\\/g,"");
-                respostaJourneyInfo = data.slice(1,-1);
-                journeyInfoJson = JSON.parse(respostaJourneyInfo); 
-                sendInformation();
-                });
-            }) 
-
-            req3.on('error', (error) => {
-                console.error(error)
-            })
-            req3.write(data);
-            req3.end();
-        /*----------------------HISTORYJOURNEY-----------------------*/    
-};
-
-function sendInformation(){
-    /*----------------------SENDINFORMATION-----------------------*/ 
-    var body = {
-        "Email": decodedArgs.Email,
-        "client_id": journeyInfoJson.took
-    };
-
-    const data = JSON.stringify(body)
-
-    const options = {
-        hostname: host,
-        path: path,
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': jsonSize(body)
-        }
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+        connection.trigger('requestSchema');
     }
 
-    const req2 = http.request(options, (res) => {
-        console.log('statusCode: ${res.statusCode}')
-
-        res.on('data', (d) => {
-        process.stdout.write(d)
-        })
-    }) 
-
-    req2.on('error', (error) => {
-        console.error(error)
-    })
-
-    req2.write(data);
-    req2.end();
-
-    /*----------------------SENDINFORMATION-----------------------*/ 
-};
-
-/*
- * POST Handler for /publish/ route of Activity.
- */
-exports.listener = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.status(200).send('Listen');
-};
+    function initialize(data) {
 
 
-/*
- * POST Handler for /publish/ route of Activity.
- */
-exports.publish = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.status(200).send('Publish');
-};
+        $('#my-select').multiSelect({
+            afterSelect: function (values) {
+                camposSelected.push(values);
+                alert(camposSelected)
+            },
+            afterDeselect: function (values) {
+                var index = camposSelected.indexOf(values);
+                camposSelected.splice(index, 1);
+                alert(camposSelected)
+            }
+        });
 
-/*
- * POST Handler for /validate/ route of Activity.
- */
-exports.validate = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-	res.status(200).send('Validate');
-};
+
+
+        console.log(data);
+        if (data) {
+            payload = data;
+        }
+
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        console.log(inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                alert('VALUE-> ' + val)
+            });
+        });
+
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+
+        connection.on('requestedSchema', function (data) {
+            // save schema
+            console.log('*** Schema ***', JSON.stringify(data['schema']));
+            var JsonParsed = data['schema'];
+            var key;
+            for (var i = 0, len = data['schema'].length; i < len; ++i) {
+                key = JsonParsed[i].key;
+                camposDE.push(key.substr(key.lastIndexOf(".") + 1));
+            }
+            camposDE.forEach(function (campo) {
+                console.log("CAMPO DA DATA EXTENSION -> " + campo);
+                $('#my-select').multiSelect('addOption', { value: campo, text: campo, index: 0, nested: 'optgroup_label' });
+            });
+        });
+    }
+
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
+    }
+
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
+    }
+
+
+    function save() {
+        var name = 'API Exemplo Sonae';
+        var endpointValue = $('#endpoint-url').val();
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+        payload.name = name;
+        //var argumentos = "[{";
+
+        /*   camposDE.forEach(function(campo) {
+               console.log("CAMPO DA DATA EXTENSION -> " + campo);
+               arguments = arguments + "\"campo\": '{{Contact.Attribute.De_DEV.Password}}
+               $('#my-select').multiSelect('addOption', { value: campo, text: campo, index: 0, nested: 'optgroup_label' });
+           });*/
+
+        payload['arguments'].execute.inArguments = [{
+            "Definition-id": '{{Context.DefinitionId}}',
+            "Endpoint": endpointValue,
+            "User": "{{Contact.Key}}",
+            "Email": '{{InteractionDefaults.Email}}',
+            "Nome": "{{Contact.Attribute.DEAudience-8e5d74c5-9549-2c66-554d-5073a2c25b60.Nome}}"
+        }];
+
+        payload['metaData'].isConfigured = true;
+
+        connection.trigger('updateActivity', payload);
+    }
+
+
+});
