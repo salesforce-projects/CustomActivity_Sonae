7c51ce36046395cd544b9bc84c5b1b8c3190120c
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-10-11 11:32:32 +0100

    Update customActivity.js

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -119,8 +119,9 @@ define([
         }];
         console.log(valoresDE);
         
-        camposSelected.forEach(function (campo) {
+        camposDE.forEach(function (campo) {
             console.log("GET DO MAPA -> " + valoresDE.get(campo));
+            console.log("VALORES DE -> " + valoresDE);
             console.log("CAMPO -> " + campo);
             jsonObject[0][campo] = "{{"+valoresDE.get(campo)+"}}";
         });
